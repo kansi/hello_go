a05fb0653497afb3b1a7b0664ef377c1ae5601fa
Vanshdeep Singh <kansi13@gmail.com>
2021-07-24 11:11:46 +0200

    Add dep to run executables

--- rubin/lib/rubin/application.ex
+++ rubin/lib/rubin/application.ex
@@ -1,19 +1,26 @@
 defmodule Rubin.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
+    app_dir = Application.app_dir(:rubin)
+
+    go_server_spec =
+      Supervisor.child_spec(
+        {MuonTrap.Daemon, ["#{app_dir}/priv/server", [], []]},
+        id: :go_hello,
+        restart: :transient
+      )
+
+    Logger.info("running server #{go_server_spec}")
+
     children = [
-      # Starts a worker by calling: Rubin.Worker.start_link(arg)
-      # {Rubin.Worker, arg}
+      go_server_spec
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Rubin.Supervisor]
     Supervisor.start_link(children, opts)
   end
