45c9f0d86da93f3158c8efced351771bb3b07c3a
Vanshdeep Singh <kansi13@gmail.com>
2021-07-24 10:31:28 +0200

    Update versioning

--- rubin/mix.exs
+++ rubin/mix.exs
@@ -4,7 +4,7 @@ defmodule Rubin.MixProject do
   def project do
     [
       app: :rubin,
-      version: "0.1.0",
+      version: version(),
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
