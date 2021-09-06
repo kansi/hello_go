60eab58cee6b2a4e2fb6877dddcf0191a938495d
Vanshdeep Singh <kansi13@gmail.com>
2021-09-06 19:56:59 +0200

    Update message

--- hello/server.go
+++ hello/server.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello from Golang 🎸",
+			"message": "Hello from Golang 🧭",
 			"pid": os.Getpid(),
 		})
 	})
